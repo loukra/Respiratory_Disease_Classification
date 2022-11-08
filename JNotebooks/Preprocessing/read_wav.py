6c115a76ceef0d9c2bc638334d5b3e0f33ddb5bf
Li <shelley.xieli@gmail.com>
2022-11-08 16:33:24 +0100

    read_wav.py

---
+++ JNotebooks/Preprocessing/read_wav.py
@@ -0,0 +1,27 @@
+"""read_wav
+    function reads wav file
+    OUTPUT:
+    vector
+    sr -> sampling rate
+"""
+
+import pandas as pd
+import librosa
+
+def _read_wav_(filename, tar_sr):
+    """_read_wav_
+
+    Args:
+        filename (_type_): filename fetched from annotation.csv filename
+        tar_sr (_type_): target sampling rate for output
+
+    Returns:
+        _type_: _description_
+    """
+    wav_path = "data/Sounds"+filename
+    ori_sr = librosa.get_samplerate(wav_path) # save the original sampling rate
+    vec, tar_sr = librosa.load(wav_path, sr=tar_sr)
+    print(f'Original sr: {ori_sr}, Target sr: {tar_sr}')
+    return vec, tar_sr
+
+
